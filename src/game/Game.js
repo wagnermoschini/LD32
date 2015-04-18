3c848c2dab8e51635dddd56c4f2d4187b2248293
Wagner <wagnermoschini@gmail.com>
2015-04-18 19:46:58 -0300

    add Alien class

--- src/game/Game.js
+++ src/game/Game.js
@@ -1,33 +1,29 @@
 var PIXI = require('pixi.js');
 var Grandma = require('./Grandma');
+var Alien = require('./Alien');
 
 var Game = function(container) {
   this.view = new PIXI.DisplayObjectContainer();
 
   this.scenario = PIXI.Sprite.fromFrame('scenario.png');
-  this.alien = PIXI.Sprite.fromFrame('alien.png');
-  this.aliens = []
+  this.alien =  new Alien();
+  this.aliens = [];
   this.grandma = new Grandma();
 
   this.view.addChild(this.scenario);
   this.view.addChild(this.grandma.view);
-  this.view.addChild(this.alien);
+  this.view.addChild(this.alien.view);
 
   this.scenario.anchor.x = 0.5;
   this.scenario.anchor.y = 0.5;
-
-  this.alien.anchor.x = 0.5;
-  this.alien.anchor.y = 0.5;
 
   this.time = 0;
 };
 
 Game.prototype.update = function() {
 
-
-  this.alien.position.x = Math.sin(this.time*0.05)*30;
-  this.alien.position.y = Math.cos(this.time*0.05)*30;
   this.time += 1;
+  this.alien.update(this.time);
 }
 
 module.exports = Game;
